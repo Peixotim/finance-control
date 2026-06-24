245f35d84693e02f8c7f1af4e829b8277b8fe08f
Peixotim <pedropeixotovz@gmail.com>
2026-06-24 17:36:41 -0300

    feat: initialize database connection on startup and use centralized env config

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import express from 'express'
-import { config } from 'dotenv'
-config()
+import express from "express";
+import { env } from "./config/env";
+import { AppDataSource } from "./infra/database";
 
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const app = express();
+const PORT = env.PORT ?? 8080;
 
 async function bootstrap() {
+  try {
+    await AppDataSource.initialize();
     app.listen(PORT, () => {
-        console.log(`API is running on port ${PORT}`)
-    })
+      console.log(`API is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
-bootstrap()
+bootstrap();
