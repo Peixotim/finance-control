5e6c84a1c4756e0a87e5e800bd0a647ad4e9c0a0
Peixotim <pedropeixotovz@gmail.com>
2026-06-24 17:36:38 -0300

    style: format database module with Prettier

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,12 +1,11 @@
-import {DataSource} from "typeorm";
-import { env } from '../config/env'
-
+import { DataSource } from "typeorm";
+import { env } from "../config/env";
 
 export const AppDataSource = new DataSource({
-    type : 'postgres',
-    username: env.DB_USER,
-    password: env.DB_PASSWORD,
-    port : Number(env.DB_PORT),
-    database: env.DB_NAME,
-    host: env.DB_HOST
-})
+  type: "postgres",
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  port: Number(env.DB_PORT),
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+});
