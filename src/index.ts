3d22985cb1d4487767faddb6343da088c8e46a8c
Peixotim <pedropeixotovz@gmail.com>
2026-06-24 21:59:45 -0300

    feat: wire up routes and register express JSON middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { env } from "./config/env";
 import { AppDataSource } from "./infra/database";
+import {routes} from "./modules/routes";
 
 const app = express();
 const PORT = env.PORT ?? 8080;
+
+app.use(express.json())
+app.use('/api/v1',routes)
 
 async function bootstrap() {
   try {
