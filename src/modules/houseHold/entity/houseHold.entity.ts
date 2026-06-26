c867bba5f13eedb68cb7bdc9b4df919256169b02
Peixotim <pedropeixotovz@gmail.com>
2026-06-26 09:18:45 -0300

    refactor(household): rebuild entity to match updated domain model

--- src/modules/houseHold/entity/houseHold.entity.ts
+++ src/modules/houseHold/entity/houseHold.entity.ts
@@ -5,24 +5,27 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   OneToMany,
-} from "typeorm";
-import { HouseHoldMember } from "../../houseHoldMember/entity/houseHoldMember.entity";
+} from 'typeorm';
+import { HouseholdMember } from '../../householdMember/entity/householdMember.entity';
+import { AccountShare } from '../../accountShare/entity/accountShare.entity';
 
-@Entity({ name: "house_hold" })
-export class HouseHold {
-  @PrimaryGeneratedColumn("uuid")
+@Entity({ name: 'household' })
+export class Household {
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @Column({ type: "varchar", nullable: false })
+  @Column({ type: 'varchar', nullable: false })
   name!: string;
 
-  //Verificar Relacionamento Antes de fazer a migration
-  @OneToMany(() => HouseHoldMember, (member) => member.houseHold)
-  houseHoldMembers!: HouseHoldMember[];
-
-  @CreateDateColumn({ name: "created_at" })
+  @CreateDateColumn({ name: 'created_at' })
   createdAt!: Date;
 
-  @UpdateDateColumn({ name: "updated_at" })
+  @UpdateDateColumn({ name: 'updated_at' })
   updatedAt!: Date;
+
+  @OneToMany(() => HouseholdMember, (m) => m.household)
+  members!: HouseholdMember[];
+
+  @OneToMany(() => AccountShare, (s) => s.household)
+  accountShares!: AccountShare[];
 }
