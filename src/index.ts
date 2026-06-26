810641b43ab1ac6c199cd5bbe08005fb4d2fc1d1
Peixotim <pedropeixotovz@gmail.com>
2026-06-26 11:26:11 -0300

    feat(security) : Add configurations of ratelimit and helmet

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express from "express";
 import { env } from "./config/env";
 import { AppDataSource } from "./infra/database";
-import {routes} from "./modules/routes";
-
+import { routes } from "./modules/routes";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
 const app = express();
 const PORT = env.PORT ?? 8080;
 
-app.use(express.json())
-app.use('/api/v1',routes)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 60,
+  message: "Too many requests from this IP. Please try again later.",
+  statusCode: 429,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(limiter);
+app.use("/api/v1", routes);
+app.set("trust proxy", true);
 
 async function bootstrap() {
   try {
